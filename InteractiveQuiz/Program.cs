3c2cf843f7bb61752b0d1a739bdc64dd57d03a2c
kimikomu <kimikomu@yahoo.com>
2017-05-18 20:54:14 -0700

    Added Readme file. Changed the quiz to be about state capitals.

--- InteractiveQuiz/Program.cs
+++ InteractiveQuiz/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InteractiveQuiz
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
+			string[] questionTopics = { "California", "Hawaii", "Kentucky" };
+			string[] answers = { "sacramento", "honolulu", "frankfort" };
+			int correct = answers.Length;
+
+
+			Console.WriteLine("State Capital Quiz");
+
+			for (int i = 0, n = answers.Length; i < n; i++)
+			{
+				Console.Write("What is the capital of " + questionTopics[i] + "? ");
+
+				string input = Console.ReadLine();
+
+				if (input.ToLower() != answers[i])
+				{
+					correct -= 1;
+				}
+			}
+
+			if (correct == 0)
+				Console.WriteLine("NONE correct!? You lose!");
+			else if (correct < answers.Length)
+				Console.WriteLine("You got " + correct + " answers correct.");
+			else if (correct == answers.Length)
+				Console.WriteLine("100%! You Win!");
+			else
+				Console.WriteLine("Something is not working in this game.");
+
+			while (true)
+			{
+				Console.Write("GAME OVER...Enter \"q\" to quit: ");
+				string q = Console.ReadLine();
+
+				if (q.ToLower() == "q")
+				{
+					break;
+				}
+			}
+
 		}
 	}
 }
---
+++ InteractiveQuiz/README.txt
@@ -0,0 +1,2 @@
+﻿# Interactive Quiz
+A small console application to practice C#. This app is a quiz that test you knowlege of the state capitals.
